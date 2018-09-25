86323624f14b479fb729ea6ebdc41354fc31354a
Tank <fuch@fullsample.com>
2018-09-25 12:43:01 +0800

    [fuchun] #Tech balance tree

--- src/main/java/com/tank/avt/Node.java
+++ src/main/java/com/tank/avt/Node.java
@@ -2,8 +2,6 @@ package com.tank.avt;
 
 import lombok.Data;
 
-import java.util.Objects;
-
 /**
  * @author fuchun
  */
@@ -14,14 +12,13 @@ public class Node {
   private Node left;
   private Node right;
   private int code;
-  private Node parent;
+
 
   public Node(final String value, int code) {
     this.value = value;
     this.code = code;
     this.right = null;
     this.left = null;
-    this.parent = null;
   }
 
   @Override
@@ -32,108 +29,84 @@ public class Node {
   public Node addNode(Node newNode) {
 
     boolean isLeft = this.code > newNode.code;
-
+    Node targetNode = null;
     if (isLeft) {
-      Node leftNode = this.searchLeftTree(this.left, newNode);
-      if (leftNode == null) {
+      if (this.left == null) {
         this.left = newNode;
-        newNode.parent = this;
+      } else {
+        targetNode = this.searchLeftTree(this.left, newNode);
       }
     } else {
-      Node rightNode = this.searchRightTree(this.right, newNode);
-      if (rightNode == null) {
+      if (this.right == null) {
         this.right = newNode;
-        newNode.parent = this;
+      } else {
+        targetNode = this.searchRightTree(this.right, newNode);
+      }
+    }
+
+    boolean isExists = targetNode != null;
+    if (isExists) {
+      if (targetNode.left == null) {
+        targetNode.left = newNode;
+      } else {
+        targetNode.right = newNode;
       }
     }
 
     return this;
   }
 
-  private boolean isTree() {
-    return !Objects.isNull(this.left) && !Objects.isNull(this.right);
-  }
 
   private Node searchLeftTree(Node leftNode, Node targetNode) {
-    if (targetNode.code == 1) {
-      System.out.println("debug");
-    }
-    if (leftNode == null) {
-      return null;
-    } else {
-      int leftValue = leftNode.code;
-      int targetValue = targetNode.code;
-      if (leftValue > targetValue) {
-        if (leftNode.left == null) {
-          //TODO ADD
-          leftNode.left = targetNode;
-          targetNode.parent = left;
-          System.out.println("add node");
-        }
-        Node rs = this.searchLeftTree(leftNode.left, targetNode);
-        if (rs != null) {
-          return rs;
-        }
+
+    int leftValue = leftNode.code;
+    int targetValue = targetNode.code;
+    if (leftValue > targetValue) {
+      if (leftNode.left == null) {
+        return leftNode;
       } else {
-
-        //TODO 这个地方需要看看是否是一定需要这样调用才行
-        Node rs = this.searchRightTree(leftNode.right, targetNode);
-        if (rs != null) {
-          return rs;
-        } else {
-          leftNode.right = targetNode;
-          targetNode.parent = leftNode;
-          return leftNode;
+        Node tmp = this.searchLeftTree(leftNode.left, targetNode);
+        if (tmp != null) {
+          return tmp;
         }
-
+      }
+    } else {
+      if (leftNode.right == null) {
+        return leftNode;
+      } else {
+        Node tmp = this.searchRightTree(leftNode.right, targetNode);
+        if (tmp != null) {
+          return tmp;
+        }
       }
     }
+
     return null;
   }
 
   private Node searchRightTree(Node rightNode, Node targetNode) {
-
-    if (rightNode == null) {
-      return null;
-    }
-    //6
     int rightValue = rightNode.code;
-    //8
     int targetValue = targetNode.code;
     if (targetValue > rightValue) {
-      Node rs = this.searchRightTree(rightNode.right, targetNode);
-      if (rs != null) {
-        return rs;
+      if (rightNode.right == null) {
+        return rightNode;
       } else {
-        System.out.println("....rigfht.....");
+        Node tmp = this.searchRightTree(rightNode.right, targetNode);
+        if (tmp != null) {
+          return tmp;
+        }
       }
     } else {
-      //左旋转
-
-
-      //TODO parentNode是root节点的时候需要特殊处理
-      System.out.println("左旋转");
-
-      Node parentNode = rightNode.parent;
-
-      Node grandFather = parentNode.parent;
-
-      //将右节点的父节点作为新节点的左节点
-      targetNode.left = parentNode;
-      parentNode.parent = targetNode;
-
-      //将
-      targetNode.right = rightNode;
-      rightNode.parent = targetNode;
-
-      grandFather.left = targetNode;
-      targetNode.parent = grandFather;
-
-      System.out.println("。。。旋转结束。。。");
-
-
-      return targetNode;
+      if (rightNode.left == null) {
+        return rightNode;
+      } else {
+        Node tmp = this.searchLeftTree(rightNode.left, targetNode);
+        if (tmp != null) {
+          return tmp;
+        }
+      }
     }
+
     return null;
   }
 
